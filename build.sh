38e3dea21bbc7fe8e149d8ea6cf53b077e5254ea
Adam Heinrich <adam@adamh.cz>
2015-07-01 13:58:47 +0200

    Change file names to match current project's configuration

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ rm luxfer.zip
 
 mv dist luxfer
 
-cp README.txt luxfer/
+cp README.md luxfer
 cp run.sh luxfer/
 
-zip luxfer luxfer/* luxfer/**/*
+zip luxfer luxfer/*
 
-rm luxfer/README.txt
+rm luxfer/README.md
 rm luxfer/run.sh
 
 mv luxfer dist
